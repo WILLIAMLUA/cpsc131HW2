d25bc0fa7fd39c09baa69d0ce9aa1f8798045893
WILLIAMLUA <98416548+WILLIAMLUA@users.noreply.github.com>
2022-09-05 22:57:04 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "vector131.h"
+
+using namespace std;
+
+void createVec(vector<vector131>&);
+void showVec(const vector<vector131>&);
+
+int main()
+{
+    vector<vector131> uVec;
+    createVec(uVec);
+    showVec(uVec);
+
+    system("pause");
+    return 0;
+}
+
+//Create Vector Function:
+void createVec(vector<vector131>& newVec){
+
+    //Variables
+    int size;
+    string nCourse;
+    string SName;
+    int Sid;
+
+    cout << "Enter Number of Students: ";
+    cin >> size;
+
+    for(int i = 0; i < size; i++){
+
+        cout << "Enter course name: ";
+        cin >> nCourse;
+        cout << "Enter student first name: ";
+        cin >> SName;
+        cout << "Enter student ID: ";
+        cin >> Sid;
+
+        vector131 newStudent(nCourse, SName, Sid);
+        newVec.push_back(newStudent);
+
+    }
+    
+    cout << endl;
+
+
+}
+
+void showVec(const vector<vector131>& newVec){
+
+    unsigned int vecSize = newVec.size();
+    
+    cout << "The following was entered: " << endl;
+
+    for (unsigned int i = 0; i < vecSize; i++){
+        cout << "Course: " << newVec[i].getCourse() << endl;
+        cout << "Student: " << newVec[i].getName() << endl;
+        cout << "ID number: " << newVec[i].getID() << endl;
+    }
+}
